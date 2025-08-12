e14c160dcc65ff1f8af9f1aa7053b9f2735155e3
Ennzo17 <ennzogabriel033@gmail.com>
2025-08-12 17:51:40 -0300

    Código Kivy: Painel de Controle da IA

---
+++ Desafio_Estrutura_de_App_Mobile_com_Kivy/screens/__init__.py
@@ -0,0 +1,6 @@
+"""
+Pacote screens - Define todas as telas do aplicativo
+
+Este arquivo vazio é necessário para que Python trate o diretório 'screens'
+como um pacote, permitindo importações relativas.
+"""
